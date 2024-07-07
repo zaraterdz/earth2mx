aa0545b5622740b4cd1c9c1e7959dd8ef0b71095
Alexis Zárate <leozaraterdz@hotmail.com>
2024-07-07 00:32:27 -0600

    initial commit para desplegar en vercel

---
+++ src/routes/+page.svelte
@@ -0,0 +1,52 @@
+<div
+  style="height: 100vh; display: flex; justify-content: center; align-items: center;"
+>
+  <div class="card" style="width: 23rem;">
+    <img src="" class="card-img-top" alt="" />
+    <div class="card-body" style="text-align:center;">
+      <h5 class="card-title">earth2.mx 🌎 está a la venta</h5>
+      <p class="card-text">
+        Comunícate conmigo vía WhatsApp o correo electrónico.
+        <br />
+        <span>👍🏻😃🌴</span>
+      </p>
+      <a
+        target="_blank"
+        href="https://wa.me/528121119899?text=Estoy%20interesado%20en%20el%20dominio%20Earth2.mx%20,negociemos!"
+        class="btn btn-primary"
+        style="background:#26C943; border:#26C943;"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-whatsapp"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"
+          />
+        </svg> WhatsApp</a
+      >
+      <a
+        target="_blank"
+        href="https://mail.google.com/mail/?view=cm&fs=1&to=someone@example.com&su=SUBJECT&body=BODY&bcc=someone.else@example.com"
+        class="btn btn-primary"
+        style="background:#E34133; border:#E34133;"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-google"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"
+          />
+        </svg> Gmail</a
+      >
+    </div>
+  </div>
+</div>
